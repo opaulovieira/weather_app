b120e30038093e7e47af0393b59e84b76010c177
opaulovieira <paulovieirasilva.1998@gmail.com>
2024-01-17 03:07:18 -0300

    feat(location): turn Location into a comparable object

--- features/locator/lib/src/model/location.dart
+++ features/locator/lib/src/model/location.dart
@@ -1,9 +1,14 @@
-base class Location {
+import 'package:equatable/equatable.dart';
+
+base class Location extends Equatable {
   const Location({
-    required this.longitude,
     required this.latitude,
+    required this.longitude,
   });
 
-  final double longitude;
   final double latitude;
+  final double longitude;
+
+  @override
+  List<Object?> get props => [latitude, longitude];
 }
