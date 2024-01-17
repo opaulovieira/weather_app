694c19878d327e89e8401d1e3c576a1d8497b273
opaulovieira <paulovieirasilva.1998@gmail.com>
2024-01-17 00:48:32 -0300

    feat(weather): create minimal viable weather page

--- lib/weather/weather_view.dart
+++ lib/weather/weather_view.dart
@@ -1,10 +1,61 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 class WeatherView extends StatelessWidget {
   const WeatherView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Clima"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              // TODO(opaulovieira): logout
+            },
+            icon: const Icon(
+              Icons.exit_to_app_rounded,
+            ),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          // TODO(opaulovieira): create class for constants
+          horizontal: 32.0,
+          vertical: 64.0,
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  // TODO(opaulovieira): use variables for State and City
+                  "State, City",
+                  style: Theme.of(context).textTheme.headlineLarge,
+                ),
+                Text(
+                  // TODO(opaulovieira): use variables for Temperature
+                  "92 °C",
+                  style: Theme.of(context).textTheme.headlineSmall,
+                ),
+              ],
+            ),
+            IconButton(
+              onPressed: () {
+                // TODO(opaulovieira): fetch location and weather data
+              },
+              icon: const Icon(
+                Icons.refresh_rounded,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
