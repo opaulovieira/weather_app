8d3697fb7436266b6a812f2653c883e9569e81ff
opaulovieira <paulovieirasilva.1998@gmail.com>
2024-01-19 06:02:39 -0300

    feat(weather): implement service for fetching weather data

--- features/weather/lib/weather.dart
+++ features/weather/lib/weather.dart
@@ -1,8 +1,6 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library;
 
-export 'src/weather_base.dart';
+export 'src/model/model.dart';
+export 'src/weather.dart';
 
 // TODO: Export any libraries intended for clients of this package.
