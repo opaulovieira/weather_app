61d65774c29d30de73db39f0dc5276cc45fed4ed
opaulovieira <paulovieirasilva.1998@gmail.com>
2024-01-19 07:24:02 -0300

    feat(weather): implement example using variable from environment

--- features/weather/example/weather_example.dart
+++ features/weather/example/weather_example.dart
@@ -1,6 +1,11 @@
-import 'package:weather/weather.dart';
+import 'package:dio/dio.dart';
+import 'package:weather/src/weather_impl.dart';
 
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+Future<void> main() async {
+  const apiKey = String.fromEnvironment('APIKEY');
+
+  final dio = Dio();
+  final weather = WeatherImpl(dio: dio, apiKey: apiKey);
+  final current = await weather.getCurrent(33.44, -94.04);
+  print(current);
 }
