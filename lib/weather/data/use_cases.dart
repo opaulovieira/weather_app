e36570b7154cc468b473c535f66679a61b9fbeb3
opaulovieira <paulovieirasilva.1998@gmail.com>
2024-01-20 10:40:39 -0300

    feat: create custom exception

--- lib/weather/data/use_cases.dart
+++ lib/weather/data/use_cases.dart
@@ -1,7 +1,7 @@
+import 'package:locator/locator.dart';
+import 'package:weather/weather/data/exceptions.dart';
 import 'package:weather/weather/data/model.dart';
 import 'package:weather/weather/data/repository.dart';
-
-final class FailedToGetWeather implements Exception {}
 
 final class GetWeatherUseCase {
   const GetWeatherUseCase({required this.repository});
@@ -9,13 +9,24 @@ final class GetWeatherUseCase {
   final WeatherRepository repository;
 
   Future<Weather> call() async {
-    final hasPermission = await repository.allowWeatherRequest();
-    final isEnabled = await repository.checkIfEnabled();
+    try {
+      final hasPermission = await repository.allowWeatherRequest();
+      final isEnabled = await repository.checkIfEnabled();
 
-    if (!isEnabled || !hasPermission) {
-      throw FailedToGetWeather();
-    } else {
-      return repository.getWeather();
+      if (!isEnabled || !hasPermission) {
+        throw FailedToGetWeather();
+      } else {
+        final weather = await repository.getWeather();
+
+        return weather;
+      }
+    } on LocatorException catch (error) {
+      switch (error) {
+        case WebBrowserHasNoPermissionAPI():
+          throw UserMayChangeWebBrowser();
+        case DeniedUntilChangedOnAppSettings():
+          throw UserShouldAllowOnAppSettings();
+      }
     }
   }
 }
